a9ba83aa21c63ef808f2103f32900e124dbfcca7
mahan mokhtari <mahanmokhtari27@gmail.com>
2023-10-22 14:45:53 +0100

    made background image

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,67 +6,74 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-            title: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  ElevatedButton(
-                      onPressed: () {
-                        print('1');
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.transparent,
-                        foregroundColor: Colors.black,
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                        elevation: 0,
+        body: Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("lib/images/homeBG.jpg"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          title: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                    onPressed: () {
+                      print('1');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.transparent,
+                      foregroundColor: Colors.black,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                      elevation: 0,
+                    ),
+                    child: Container(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        "near me",
+                        style: TextStyle(fontSize: 20),
                       ),
-                      child: Container(
-                        alignment: Alignment.centerRight,
-                        child: Text(
-                          "near me",
-                          style: TextStyle(fontSize: 20),
-                        ),
-                      )),
-                  ElevatedButton(
-                      onPressed: () {
-                        print('2');
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.transparent,
-                        foregroundColor: Colors.black,
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                        elevation: 0,
+                    )),
+                ElevatedButton(
+                    onPressed: () {
+                      print('2');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.transparent,
+                      foregroundColor: Colors.black,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                      elevation: 0,
+                    ),
+                    child: Container(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "search",
+                        style: TextStyle(fontSize: 20),
                       ),
-                      child: Container(
-                        alignment: Alignment.center,
-                        child: Text(
-                          "search",
-                          style: TextStyle(fontSize: 20),
-                        ),
-                      )),
-                  ElevatedButton(
-                      onPressed: () {
-                        print('3');
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.transparent,
-                        foregroundColor: Colors.black,
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                        elevation: 0,
+                    )),
+                ElevatedButton(
+                    onPressed: () {
+                      print('3');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.transparent,
+                      foregroundColor: Colors.black,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                      elevation: 0,
+                    ),
+                    child: Container(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "recents",
+                        style: TextStyle(fontSize: 20),
                       ),
-                      child: Container(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "recents",
-                          style: TextStyle(fontSize: 20),
-                        ),
-                      )),
-                ])),
-        body: Container());
+                    )),
+              ])),
+    ));
   }
 }
